f82d982092a9a63e068a805d1759f4c2831eb88f
chuanky <shicq01@gmail.com>
2021-05-20 23:14:18 +0800

    Update JwtService.java
    加修改计划注释

--- src/main/java/com/example/oAuth2Demo/service/JwtService.java
+++ src/main/java/com/example/oAuth2Demo/service/JwtService.java
@@ -22,7 +22,11 @@ public class JwtService {
     @Autowired
     UserRepository userRepo;
 
-    private final String SECRET_KEY = "thisIsAnImportantSecretForGeneratingJWT";
+    /**
+    TODO: 该密码用于加密和解密JWT(JSON Web Token)，如果想废除已发放的JWT，修改次密码即可，
+     该密码不应以这种形式保存在源代码中，需要找一下该密码合适的保存方式，例如：可以保存在数据库中。
+     */
+    private final String SECRET_KEY = "thisIsAnImportantSecretForGeneratingJWT1";
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -57,12 +61,19 @@ public class JwtService {
         return createToken(claims, userDetails.getUsername());
     }
 
+    /**
+     TODO: 对于移动端用户来说，需要允许长时间不使用应用下次使用也不需要登录，所以发放token的逻辑需要修改，
+      需要和前端交流，是否能够获取设备的ID或者设备名称，如果可以则在claims中加入该信息，并修改validateToken函数
+     */
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
+    /**
+     TODO: 如果是带有设备名称的token，检验token过期的逻辑不执行
+     */
     public Boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
